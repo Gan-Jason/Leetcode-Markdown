dee4d1274e339200cf6e2b74acfa13ae5895e109
Jason Escobar <binuogan@gmail.com>
2019-06-04 12:48:17 +0800

    rm

--- LeetCode/CreateTree.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-  struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-  };
