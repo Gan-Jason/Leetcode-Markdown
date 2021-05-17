5d2412cfd55e4598e73f2db7192875c8debe0e4b
Jason-Gan <48959194+Gan-Jason@users.noreply.github.com>
2021-05-17 21:25:35 +0800

    Update 26-tp.java

--- LeetCode/Array/26-tp.java
+++ LeetCode/Array/26-tp.java
@@ -1,1 +1,17 @@
+//同向双指针法，
 
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length<=1){
+            return nums.length;
+        }
+        int i=0,j=0;
+        while(j<nums.length){
+            if(i==0||nums[j]!=nums[i-1]){     //模版
+                nums[i++]=nums[j];
+            }
+            j++;
+        }
+        return i;
+    }
+}
