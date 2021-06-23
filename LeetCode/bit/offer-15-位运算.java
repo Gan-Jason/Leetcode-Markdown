ee7a247f9fde728c96c40b62931fe5d7efb1c4e5
Jason-Gan <48959194+Gan-Jason@users.noreply.github.com>
2021-06-24 05:50:38 +0800

    Create offer-15-位运算.java

---
+++ LeetCode/bit/offer-15-位运算.java
@@ -0,0 +1,22 @@
+//这题有两个思路，一个是跟1求与，一个是跟2的i次方求与，即一个除法，一个乘法
+//需要注意的是，要把n当作无符号数来看，不然会出现负数的情况，所以做除法的时候要用无符号右移
+//2的i次方：1<<i
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int ans=0;
+        // do{
+        //     if((n&1)==1){
+        //         ans++;
+        //     }
+        //     n>>>=1;    //转为正数
+        // }while(n>=1);
+        for(int i=0;i<32;i++){
+            if((n&(1<<i))!=0){    //这里要注意的是，不为0，因为1作左移不一定是求与后不一定是1，
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
