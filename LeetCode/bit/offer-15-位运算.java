741761af3cb84cc9f6945b543487e41bdeb8b4f6
Jason-Gan <48959194+Gan-Jason@users.noreply.github.com>
2021-06-24 05:59:47 +0800

    Update offer-15-位运算.java

--- LeetCode/bit/offer-15-位运算.java
+++ LeetCode/bit/offer-15-位运算.java
@@ -1,6 +1,8 @@
 //这题有两个思路，一个是跟1求与，一个是跟2的i次方求与，即一个除法，一个乘法
 //需要注意的是，要把n当作无符号数来看，不然会出现负数的情况，所以做除法的时候要用无符号右移
 //2的i次方：1<<i
+//还有一种o(logn)方法，就是用n&(n-1)，可以把n的第一位变为0，n=n&(n-1)
+
 
 public class Solution {
     // you need to treat n as an unsigned value
